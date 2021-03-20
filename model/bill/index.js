033ff9aef49859f17a24cb4cf133064fef42b24f
rayhomie <1572801584@qq.com>
2021-03-20 14:04:44 +0800

    账单模块完成

--- model/bill/index.js
+++ model/bill/index.js
@@ -30,40 +30,40 @@ class BillController {
     })
   }
 
-  // updateOne(json1, json2) {
-  //   return new Promise((resolve, reject) => {
-  //     BillModel.updateOne(
-  //       json1,
-  //       { $set: json2 },
-  //       (err, docs) => {
-  //         if (err) {
-  //           resolve({ err, code: 1, info: '修改失败' });
-  //           return;
-  //         }
-  //         if (docs.n === 1 && docs.nModified === 1) {
-  //           resolve({ docs, code: 0, info: '修改成功' })
-  //         } else {
-  //           resolve({ docs, code: 1, info: '修改失败' })
-  //         }
-  //       })
-  //   })
-  // }
+  updateOne(json1, json2) {
+    return new Promise((resolve, reject) => {
+      BillModel.updateOne(
+        json1,
+        { $set: json2 },
+        (err, docs) => {
+          if (err) {
+            resolve({ err, code: 1, info: '修改失败' });
+            return;
+          }
+          if (docs.n === 1 && docs.nModified === 1) {
+            resolve({ docs, code: 0, info: '修改成功' })
+          } else {
+            resolve({ docs, code: 1, info: '修改失败' })
+          }
+        })
+    })
+  }
 
-  // deleteOne(json) {
-  //   return new Promise((resolve, reject) => {
-  //     BillModel.remove(json, (err, docs) => {
-  //       if (err) {
-  //         resolve({ err, code: 1, info: '删除失败' })
-  //         return
-  //       }
-  //       if (docs.n === 1 && docs.deletedCount === 1) {
-  //         resolve({ docs, code: 0, info: '删除成功' })
-  //       } else {
-  //         resolve({ docs, code: 1, info: '删除失败' })
-  //       }
-  //     })
-  //   })
-  // }
+  deleteOne(json) {
+    return new Promise((resolve, reject) => {
+      BillModel.remove(json, (err, docs) => {
+        if (err) {
+          resolve({ err, code: 1, info: '删除失败' })
+          return
+        }
+        if (docs.n === 1 && docs.deletedCount === 1) {
+          resolve({ docs, code: 0, info: '删除成功' })
+        } else {
+          resolve({ docs, code: 1, info: '删除失败' })
+        }
+      })
+    })
+  }
 }
 
 module.exports = new BillController()
--- routes/api/bill/index.js
+++ routes/api/bill/index.js
@@ -1,12 +1,14 @@
 const router = require('koa-router')()
 const bill = require('../../../model/bill/index')
 
+//获取当前用户的所有账单可分页
 router.get('/list', async (ctx, next) => {
   const param = ctx.request.query
   const res = await bill.findList({ user_id: ctx.state.userinfo.id, ...param })
   ctx.body = res
 })
 
+// 添加一条账单记录
 router.post('/add', async (ctx, next) => {
   const { category_id, amount, bill_time, remark } = ctx.request.body
   const res = await bill.insertOne(
@@ -20,17 +22,31 @@ router.post('/add', async (ctx, next) => {
   ctx.body = res
 })
 
+// 更新某条账单的信息
 router.post('/update', async (ctx, next) => {
-
+  const { id, ...restParam } = ctx.request.body
+  if (!id) {
+    ctx.body = { info: '请指定账单id', code: 1 };
+    return;
+  }
+  const res = await bill.updateOne(
+    {
+      user_id: ctx.state.userinfo.id,
+      _id: id
+    },
+    restParam
+  )
+  ctx.body = res
 })
 
+// 删除当前用户的指定id的某条账单
 router.delete('/delete', async (ctx, next) => {
-
+  const { id } = ctx.request.body
+  const res = await bill.deleteOne({
+    user_id: ctx.state.userinfo.id,
+    _id: id
+  })
+  ctx.body = res
 })
-
-
-
-
-
 
 module.exports = router.routes()
