b7944019d171f72c3a13c7b77ee1e1ef47708f98
rayhomie <1572801584@qq.com>
2021-03-24 10:36:43 +0800

    添加email

--- dbs/user/index.js
+++ dbs/user/index.js
@@ -17,5 +17,6 @@ var User = new mongoose.Schema({ // 实列化mongoose映射
     required: true,
   },
   mobile_number: String,
+  email: String,
 });
 module.exports = mongoose.model('User', User, 'user') // 创建一个mongoose对象模型
