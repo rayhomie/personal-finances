697e27fb5a3b0585b4ebf142fdd453cb23a7949a
rayhomie <1572801584@qq.com>
2021-03-27 20:35:07 +0800

    预算

--- routes/api/budget/index.js
+++ routes/api/budget/index.js
@@ -23,7 +23,6 @@ router.post('/add', async (ctx, next) => {
         create_time: { $gte: startMonth, $lt: endMonth }
     })
     if (query.docs) {
-        console.log(query.docs)
         // 当月已经设置预算
         const res = await budget.updateOne({ _id: query.docs._id }, param)
         ctx.body = res
@@ -39,6 +38,25 @@ router.post('/add', async (ctx, next) => {
 
 // 删除当月预算
 router.delete('/delete', async (ctx, next) => {
+    const user_id = ctx.state.userinfo.id
+    const startMonth = moment().startOf('month').unix()
+    const endMonth = moment().endOf('month').add(1, 'day').startOf('day').unix()
+    const query = await budget.findOne({
+        user_id,
+        create_time: { $gte: startMonth, $lt: endMonth }
+    })
+    if (query.docs) {
+        const res = await budget.deleteOne({
+            user_id,
+            _id: query.docs._id
+        })
+        ctx.body = res
+    } else {
+        ctx.body = {
+            info: '无预算进行删除操作',
+            code: 1
+        }
+    }
 
 })
 
